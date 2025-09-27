66c3b24a66cda346ce1ce070c939041beb688d3a
MCBponta <k.sakaguchi@macbee.co.jp>
2025-09-27 16:46:06 +0900

    add : テストビルド　2022.3 LTS

---
+++ Il2CppOutputProject/Source/il2cppOutput/Wingman_CodeGen.c
@@ -0,0 +1,47 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mF956B4D9EA44ECB10E727C017158BFBEDADE628D (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m2AED40019F1DC81D9D5889809EB4A5FA73B5D5B8 (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mF956B4D9EA44ECB10E727C017158BFBEDADE628D,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_m2AED40019F1DC81D9D5889809EB4A5FA73B5D5B8,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	6009,
+	4152,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Wingman_CodeGenModule;
+const Il2CppCodeGenModule g_Wingman_CodeGenModule = 
+{
+	"Wingman.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
